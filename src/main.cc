dc405b85a92b36218f7a470c11df87e72df164fa
samfromcadott <samahstan@gmail.com>
2023-12-05 07:42:44 -0600

    Made inprovements to moon generation

--- src/main.cc
+++ src/main.cc
@@ -33,25 +33,7 @@ int main(void) {
 	camera.position = (Vector3){ 0.0f, 0.0f, r };
 	camera.target = (Vector3){ r, r, r };
 
-
-	// SDF planet = sphere(r - 1);
-	// // planet = add( planet, box(5, 5, 1) );
-	// // SDF planet = box(5, 5, 2);
-	// // planet = transform( planet, mat4::RotateXYZ( vec3(0, 45, 45) ) * mat4::Translate(5, 5, 5) );
-	// // SDF::sphere(grid, {8,8,8}, 6.0, -1); // Generate a sphere using an SDF
-	//
-	// for (int i = 0; i < 50; i++) {
-	// 	float radius = GetRandomValue(1, 5);
-	// 	SDF crater = sphere( radius );
-	// 	crater = transform( crater, mat4::Translate(r, 0, 0) );
-	// 	vec3 rotation( GetRandomValue(0, 360), GetRandomValue(0, 360), GetRandomValue(0, 360) );
-	// 	crater = transform( crater, mat4::RotateXYZ(rotation) );
-	//
-	// 	planet = sub(crater, planet);
-	// }
-	//
-	// planet = transform( planet, mat4::Translate(r, r, r) );
-	SDF planet = asteroid();
+	SDF planet = moon();
 
 	// Set voxel weights
 	for (int x = 0; x < grid.total_size().x; x++)
@@ -60,15 +42,6 @@ int main(void) {
 		float w = planet( vec3(x, y, z) );
 		grid(x, y, z).weight = Clamp(w * 64, -128, 127 );
 	}
-
-	// Generate the mesh
-	// std::vector< Brutus::Mesh > meshes;
-	// for (int x = 0; x < grid.chunk_size().x; x++)
-	// for (int y = 0; y < grid.chunk_size().y; y++)
-	// for (int z = 0; z < grid.chunk_size().z; z++) {
-	// 	std::cout << x << " " << y << " " << z << '\n';
-	// 	meshes.push_back( grid.generate_mesh(x, y, z) );
-	// }
 
 	while ( !WindowShouldClose() ) {
 		// get_edit(camera, grid);
@@ -80,14 +53,6 @@ int main(void) {
 
 			BeginMode3D(camera);
 				render_mesh(grid.generate_mesh(0, 0, 0));
-				// render_mesh(grid.generate_mesh(0, 0, 1));
-				// render_mesh(grid.generate_mesh(0, 1, 0));
-				// render_mesh(grid.generate_mesh(0, 1, 1));
-				// render_mesh(grid.generate_mesh(1, 0, 0));
-				// render_mesh(grid.generate_mesh(1, 0, 1));
-				// render_mesh(grid.generate_mesh(1, 1, 0));
-				// render_mesh(grid.generate_mesh(1, 1, 1));
-				// for (auto& mesh : meshes) render_mesh(mesh);
 
 				// Draw the bounds of the grid
 				const Color clear_grey = {224, 224, 224, 128};
--- src/planet.cc
+++ src/planet.cc
@@ -1,28 +1,40 @@
+#include <random>
+
 #include <brutus/brutus.h>
 #include <raylib-cpp.hpp>
 
 #include "planet.hh"
 
-const float r = (Brutus::Chunk::size / 2) - 1;
-const float min_radius = 12;
+const float r = Brutus::Chunk::size / 2;
+const float min_radius = 10;
+const float max_radius = 12;
 const float crater_coeff = 1;
 const int asteroid_count = 3;
 
+std::random_device rd;
+std::mt19937 mt( rd() );
+std::uniform_real_distribution<float> planet_size(min_radius, max_radius);
+std::uniform_real_distribution<float> crater_size(3, 5);
+std::uniform_real_distribution<float> crater_angle(0, 360);
+
 SDF moon() {
-	float radius = GetRandomValue(min_radius, r-1);
+	float radius = planet_size(mt);
 	SDF planet = sphere(radius);
 
+	SDF craters = sphere(0);
 	int n = radius * crater_coeff;
 	for (int i = 0; i < n; i++) {
-		float s = GetRandomValue(6, radius / 12);
+		float s = crater_size(mt);
 		SDF crater = sphere(s);
 		crater = transform( crater, mat4::Translate(radius + 1, 0, 0) );
-		vec3 rotation( GetRandomValue(0, 360), GetRandomValue(0, 360), GetRandomValue(0, 360) );
+		vec3 rotation( crater_angle(mt), crater_angle(mt), crater_angle(mt) );
 		crater = transform( crater, mat4::RotateXYZ(rotation) );
 
-		planet = add(crater, planet, 4.0);
-		planet = sub( transform( crater, mat4::Scale(1.1, 1.1, 1.1) ), planet, 2.0 );
+		craters = add(craters, crater);
 	}
+
+	planet = add(craters, planet, 3.0);
+	planet = sub( transform( craters, mat4::Scale(1.1, 1.1, 1.1) ), planet, 1.0 );
 
 	planet = transform( planet, mat4::Translate(r, r, r) );
 
